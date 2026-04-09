d45dffaef9f503a37983391beaaf3118f0103c9d
Siddhanth Madhusudhan <sid210@ymail.com>
2026-04-09 07:51:54 +0530

    feat: find weather

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,2 +1,14 @@
-@Library('platform-lib') _
-pythonServicePipeline()
+@Library('platform-pipelines') _
+
+withCredentials([
+  string(credentialsId: 'weather-api-key', variable: 'API_KEY')
+]) {
+
+  pythonService(
+    appName: "weather-service",
+    port: "5000",
+    envVars: [
+      "API_KEY": API_KEY
+    ]
+  )
+}
--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+API_KEY = os.getenv("API_KEY")
+
 @app.route("/")
 def home():
-    return "Hello from Platform Demo v2"
+    return "Weather service is running"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+@app.route("/weather")
+def get_weather():
+    city = request.args.get("city", "Chennai")
+
+    if not API_KEY:
+        return jsonify({"error": "API_KEY not configured"}), 500
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        return jsonify({
+            "city": city,
+            "temperature": data["main"]["temp"],
+            "weather": data["weather"][0]["description"]
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
